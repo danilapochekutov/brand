0044ac1ba07dfb05e58b136372b7efd3461ff32e
danilapochekutov <danya270203@gamil.com>
2024-08-31 00:25:11 +0300

    initial commit

---
+++ js/modules/wow.js
@@ -0,0 +1,12 @@
+function animation() {
+    let wow = new WOW({
+		boxClass: "wow",
+		animateClass: "animate__animated",
+		offset: 0,
+		mobile: true,
+		live: true,
+		// callback: function (box) {}
+    })
+    wow.init();
+}
+export default animation;
